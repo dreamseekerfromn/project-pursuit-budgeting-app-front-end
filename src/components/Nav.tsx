3c69169fb6aab34364b88a3194d0f1ea46981c55
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-10-18 09:07:21 -0400

    midjourney; still working for basic components

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,25 @@
-export default function Nav(){
-    return(<></>)
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <header>
+      <article>
+        <h1>
+          <Link to="/">
+            Budget App
+          </Link>
+        </h1>
+      </article>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/income">Income</Link>
+          </li>
+          <li>
+            <Link to="/spening">Spending</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
